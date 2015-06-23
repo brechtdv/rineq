ae3a098c71b4b68e7328a995b80c9edc27c90fdb
saveria <swillimes@yahoo.com>
2015-06-24 01:46:19 +0200

    Create confint.hci.R

---
+++ R/confint.hci.R
@@ -0,0 +1,13 @@
+confint.hci <-
+function(object, parm = NULL, level = 0.95, ...)
+    {
+    if (!any(class(object) == 'hci')) stop("Object is not of class hci")
+	parm <- NULL
+    a <- (1 - level) / 2
+    a <- c(a, 1 - a)
+    pct <- stats:::format.perc(a, 3)
+    fac <- qnorm(a)
+    ci <- object$concentrationIndex + fac * sqrt(object$variance) 
+    return(ci)
+    }
+
