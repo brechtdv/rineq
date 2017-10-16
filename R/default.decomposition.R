4a86ec25eb39a90825b8b60a63b6f87f1ab61bbf
Brecht <brechtdv@gmail.com>
2017-10-16 11:49:25 +0200

    drop "default" method

--- R/default.decomposition.R
+++
@@ -1,6 +0,0 @@
-default.decomposition <-
-function(object) {
-    if (class(object) != "decomposition") stop("Object is not of class decomposition")
-    unlist(object)
-}
-
--- R/default.hci.R
+++
@@ -1,6 +0,0 @@
-default.hci <-
-function(object) {
-    if (class(object) != "hci") stop("Object is not of class hci")
-    unlist(object)
-}
-
