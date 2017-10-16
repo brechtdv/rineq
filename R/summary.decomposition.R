9ae578e34b0c17128683bdf5caaaeb4b589f23d5
Brecht <brechtdv@gmail.com>
2017-10-16 12:22:27 +0200

    fix mismatch

--- R/summary.decomposition.R
+++ R/summary.decomposition.R
@@ -1,19 +1,19 @@
 summary.decomposition <-
 function(object, ...) {
-    if (class(object) != "decomposition") stop("Object is not of class decomposition")
-    cat("Overall CI:", concentration_index(object$overall_ci), "\n")
-    cat("95% confidence interval:", confint(object$overall_ci),"\n")
-    if (object$outcome_corrected) cat("(based on a corrected value)\n")
-    cat("\n")
-    
-    cat("Decomposition:\n")
-    result <- data.frame(object$contribution)
-    names(result) <- "Contribution (%)"
-    result$"Concentration Index" <- c(NA, object$partial_cis)
-    result$"lower 5%" <- c(NA, object$confints[1,])
-    result$"upper 5%" <- c(NA, object$confints[2,])
-    result$Corrected <- ""
-    result$Corrected[object$corrected_coefficients] <- "yes"
-    result$Corrected[!object$corrected_coefficients] <- "no"
-    (result)
+  if (class(object) != "decomposition") stop("Object is not of class decomposition")
+  cat("Overall CI:", concentration_index(object$overall_ci), "\n")
+  cat("95% confidence interval:", confint(object$overall_ci),"\n")
+  if (object$outcome_corrected) cat("(based on a corrected value)\n")
+  cat("\n")
+  
+  cat("Decomposition:\n")
+  result <- data.frame(object$contribution)
+  names(result) <- "Contribution (%)"
+  result$"Concentration Index" <- c(NA, object$partial_cis)
+  result$"lower 5%" <- c(NA, object$confints[1,])
+  result$"upper 5%" <- c(NA, object$confints[2,])
+  result$Corrected <- ""
+  result$Corrected[object$corrected_coefficients] <- "yes"
+  result$Corrected[!object$corrected_coefficients] <- "no"
+  return(result)
 }
--- R/summary.hci.R
+++ R/summary.hci.R
@@ -1,10 +1,10 @@
 summary.hci <-
-function(x, ...) {
-    if (!any(class(x) == 'hci')) stop("Object is not of class hci")
+function(object, ...) {
+    if (!any(class(object) == 'hci')) stop("Object is not of class hci")
     cat("Call:\n")
-    print(x$call)
-    cat("\nType of Concentration Index:\n", x$type,"\n")
-    cat("\nHealth Concentration Index:\n", x$concentration_index,"\n\n")
-    cat("Variance:\n", x$variance, "\n\n")
-    cat("95% Confidence Interval:\n", confint(x), "\n")
+    print(object$call)
+    cat("\nType of Concentration Index:\n", object$type,"\n")
+    cat("\nHealth Concentration Index:\n", object$concentration_index,"\n\n")
+    cat("Variance:\n", object$variance, "\n\n")
+    cat("95% Confidence Interval:\n", confint(object), "\n")
 }
